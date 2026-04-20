05222f1dd385e5ab66bd9a4a7cce55f468eca307
Litoxam <litobernier@gmail.com>
2026-04-20 16:09:31 +0200

    Feat: function check_if_comand_exists - checks if a command exists, using the stat function

--- parsing_user_input.c
+++ parsing_user_input.c
@@ -42,4 +42,22 @@ char **parsing_user_input(char *line)
 
 }
 
+/**
+ * check_if_command_exists - use the stat func to check if cmd exists
+ * @arg: string - tokens[0] in the main function
+ *
+ * Return: int - 0 if it exists - 1 if it doesn't
+ */
+
+int check_if_command_exists(*arg)
+{
+	struct stat st;
+
+	if (stat(arg, &st) == 0)
+		return (0);
+	else
+		return (1);
+
+}
+
 
