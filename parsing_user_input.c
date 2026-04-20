c7df82d73c1b6d183ae3492bd9d2753f13a92230
Litoxam <litobernier@gmail.com>
2026-04-20 18:46:48 +0200

    Feat: deleted the checker function into the parsing_user_input.c file, and created a cmd_checker.c file with its function inside

--- parsing_user_input.c
+++ parsing_user_input.c
@@ -41,23 +41,3 @@ char **parsing_user_input(char *line)
 	return (tokens);
 
 }
-
-/**
- * check_if_command_exists - use the stat func to check if cmd exists
- * @arg: string - tokens[0] in the main function
- *
- * Return: int - 0 if it exists - 1 if it doesn't
- */
-
-int check_if_command_exists(*arg)
-{
-	struct stat st;
-
-	if (stat(arg, &st) == 0)
-		return (0);
-	else
-		return (1);
-
-}
-
-
