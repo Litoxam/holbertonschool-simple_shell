4e48b63e168bc94f31ab9af0fcb8d4508214489c
Litoxam <litobernier@gmail.com>
2026-04-20 15:52:32 +0200

    Feat: Parse the user input, every tokens stocked into an array of strings

---
+++ parsing_user_input.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "main.h"
+
+/**
+ * parsing_user_input - split an input into tokens
+ * @line: string - string written by the user
+ *
+ * Return: array of strings - array of every arguments
+ */
+
+char **parsing_user_input(char *line)
+{
+	int size_of_token = 80;
+	int i = 0;
+	struct stat st;
+
+	char **tokens = malloc(sizeof(char *) * size_of_token);
+
+	if (tokens == NULL)
+	{
+		fprintf(stderr, "Allocation failed");
+		exit(EXIT_FAILURE);
+	}
+
+	char *token = strtok(line, " ");
+
+	if (token == NULL)
+	{
+		tokens[0] = NULL;
+		return (tokens);
+	}
+
+	while (token != NULL)
+	{
+		tokens[i] = token;
+		i++;
+		token = strtok(NULL, " ");
+	}
+	tokens[i] = NULL;
+	return (tokens);
+
+}
+
+
