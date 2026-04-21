cb4c812d9ff8dc40d4ed7a253e73c448466a9735
Litoxam <litobernier@gmail.com>
2026-04-21 11:06:29 +0200

    Feat: implement get_cmd_path function

---
+++ get_cmd_path.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * get_cmd_path - find the cmd in the PATH
+ * @arg: char - first arg read by getline
+ * Return: return the full_path if found - else return NULL
+ */
+
+char *get_cmd_path(char *arg)
+{
+
+	char *env;
+	char *env_copy;
+	char *dir;
+	char *full_path;
+
+	env = getenv("PATH");
+	env_copy = strdup(env);
+	dir = strtok(path_copy, ":");
+
+	while (dir != NULL)
+	{
+
+		full_path = malloc(strlen(dir) + strlen(arg) + 2);
+		if (full_path == NULL)
+		{
+			free(env_copy);
+			return (NULL);
+		}
+		if (full_path != NULL)
+		{
+			strcpy(full_path, dir);
+			strcat(full_path, "/");
+			strcat(full_path, arg);
+		}
+		if (stat(full_path, &st) == 0)
+		{
+			free(env_copy);
+			return (full_path);
+		}
+		else
+			{
+				free(full_path);
+				dir = strtok(NULL, ":");
+			}
+	}
+	free(env_copy);
+	return (NULL);
+}
