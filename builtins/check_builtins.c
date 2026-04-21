8bc9cf33d59b741b6c1574476196a407207b96c5
Guillaume Vaudet <guillaumevaudet.pro@gmail.com>
2026-04-21 13:41:17 +0200

    feat: implement the handle exit function with associated functions and struct

---
+++ builtins/check_builtins.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * check_builtins - checks if cmd passed by the user is a builtins functions
+ * and roots to the associated function
+ * @args: pointer to array of string - args passed to our shell
+ * Return: int - (0) if success through the pointed function -
+ * (-1) if don't match
+ */
+int check_builtins(char **args)
+{
+	int i;
+
+	i = 0;
+
+	builtin_t array_of_builtins[] = {
+		{"exit", handle_exit},
+		{NULL, NULL}
+	};
+
+	while (array_of_builtins[i].name != NULL && array_of_builtins[i].f != NULL)
+	{
+		if (strcmp(array_of_builtins[i].name, args[0]) == 0)
+			return (array_of_builtins[i].f(args));
+		i++;
+	}
+
+	return (-1);
+}
--- main.h
+++ main.h
@@ -12,11 +12,20 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+/* Structures */
+typedef struct builtin_s
+{
+	char *name;
+	int (*f)(char **args);
+} builtin_t;
+
 extern char **environ;
 
 /*prototypes*/
 char **parsing_user_input(char *line);
 int check_if_command_exists(char *arg);
 int execute_cmd_line(char **args);
+int handle_exit(char **args);
+int _atoi(char *s);
 
 #endif
