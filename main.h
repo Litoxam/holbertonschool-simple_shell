fd1c7d5bacd42909673ce86a595c018acd48554a
Litoxam <litobernier@gmail.com>
2026-04-21 20:15:43 +0200

    doc: Wrote the description of structure in main.h, in order to respect Betty Style

--- main.h
+++ main.h
@@ -12,12 +12,30 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+/* Structures */
+
+/**
+ * struct builtin_s - Built-ins structure
+ *
+ * @name: name of the command built-in
+ * @f: function pointer handling the command
+ */
+
+typedef struct builtin_s
+{
+	char *name;
+	int (*f)(char **args);
+} builtin_t;
+
 extern char **environ;
 
 /*prototypes*/
 char **parsing_user_input(char *line);
 int check_if_command_exists(char *arg);
 int execute_cmd_line(char **args);
+int handle_exit(char **args);
+int check_builtins(char **args);
+int _atoi(char *s);
 char *get_cmd_path(char *arg);
 
 #endif
