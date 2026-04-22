86757d53a1f73f58694d4a35a19ee61d175e0cbf
Litoxam <litobernier@gmail.com>
2026-04-22 21:30:12 +0200

    feat: Looks for the path in environ without using strtok

--- get_cmd_path.c
+++ get_cmd_path.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * add_arg_to_path - add a / and arg at the end of a path
+ * @arg: char - first arg read by getline
+ * @full_path: string - space allocated for full path
+ * @dir: string - directory to check
+ * Return: nothing
+ */
+
+
+void add_arg_to_path(char *full_path, char *dir, char *arg)
+{
+		strcpy(full_path, dir);
+		strcat(full_path, "/");
+		strcat(full_path, arg);
+}
+
+/**
  * get_cmd_path - find the cmd in the PATH
  * @arg: char - first arg read by getline
  * Return: return the full_path if found - else return NULL
@@ -8,44 +24,46 @@
 
 char *get_cmd_path(char *arg)
 {
-
-	char *env;
-	char *env_copy;
-	char *dir;
-	char *full_path;
-	struct stat st;
-
+	char *env, *env_copy, *dir, *full_path;
+	char *start, *end;
 
 	env = _getenv("PATH");
 	env_copy = strdup(env);
-	dir = strtok(env_copy, ":");
 
-	while (dir != NULL)
+	if (!env_copy)
+		return (NULL);
+
+	start = env_copy;
+	while (start != NULL)
 	{
+		end = strchr(start, ':');
+		if (end != NULL)
+			*end = '\0';
+		if (*start == '\0') /* :: case*/
+			dir = ".";
+		else
+			dir = start;
 
 		full_path = malloc(strlen(dir) + strlen(arg) + 2);
-		if (full_path == NULL)
+
+		if (full_path)
 		{
-			free(env_copy);
-			return (NULL);
-		}
-		if (full_path != NULL)
-		{
-			strcpy(full_path, dir);
-			strcat(full_path, "/");
-			strcat(full_path, arg);
-		}
-		if (stat(full_path, &st) == 0)
+			add_arg_to_path(full_path, dir, arg);
+
+		if (access(full_path, X_OK) == 0) /*use of access instead of stat*/
 		{
 			free(env_copy);
 			return (full_path);
 		}
-		else
-			{
-				free(full_path);
-				dir = strtok(NULL, ":");
-			}
+
+		free(full_path);
+		}
+		if (end == NULL)
+			break;
+		start = end + 1;
 	}
 	free(env_copy);
 	return (NULL);
 }
+
+
