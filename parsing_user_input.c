8cbaff82afc0263298e48e4f9ee43903a9c9f78d
Litoxam <litobernier@gmail.com>
2026-04-22 22:37:08 +0200

    feat: Parsing the input without using strtok

--- parsing_user_input.c
+++ parsing_user_input.c
@@ -3,32 +3,28 @@
 /**
  * parsing_user_input - split an input into tokens
  * @line: string - string written by the user
- *
  * Return: array of strings - array of every arguments
  */
 
 char **parsing_user_input(char *line)
 {
 	int i = 0, len = 0, size_of_token = 80;
-	char *ptr;
-	char **tokens;
+	char *ptr, *end;
+	char **tokens, **tmp;
 
 	tokens = malloc(sizeof(char *) * size_of_token);
-	if (tokens == NULL)
-	{
-		fprintf(stderr, "Allocation failed");
-		exit(EXIT_FAILURE);
-	}
 	len = strlen(line);
 	if (len > 0 && line[len - 1] == '\n')
 		line[len - 1] = '\0';
 
+	if (tokens == NULL)
+		exit(EXIT_FAILURE);
 	ptr = line;
 	while (*ptr != '\0')
 	{
 		while (*ptr == ' ' || *ptr == '\t')
-			ptr++;
-		if (*ptr == '\0')
+			ptr++; /*ignores extra spaces*/
+		if (*ptr == '\0') /*end of line*/
 			break;
 
 		tokens[i] = ptr;
@@ -36,15 +32,19 @@ char **parsing_user_input(char *line)
 		if (i >= size_of_token) /*if the buffer isn't big enough, realloc*/
 		{
 			size_of_token += 80;
-			tokens = realloc(tokens, size_of_token * sizeof(char *));
+			tmp = realloc(tokens, size_of_token * sizeof(char *));
+			if (tmp == NULL)
+				exit(EXIT_FAILURE);
+			tokens = tmp;
 		}
-		while (*ptr != ' ' && *ptr != '\t' && *ptr != '\0')
-			ptr++;
-		if (*ptr != '\0')
+		end = strpbrk(ptr, " \t"); /*looks for separator after a token*/
+		if (end) /*if separator found*/
 		{
-			*ptr = '\0';
-			ptr++;
+			*end = '\0';
+			ptr = end + 1;
 		}
+		else
+			break;
 	}
 	tokens[i] = NULL;
 	return (tokens);
