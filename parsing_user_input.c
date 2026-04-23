ba3d6f4a0526ce659a4c5bc944205ffac0744fca
Litoxam <litobernier@gmail.com>
2026-04-23 14:02:58 +0200

    hotfix: Reversed, use of in get_cmd_path.c and parsing_user_input.c

--- parsing_user_input.c
+++ parsing_user_input.c
@@ -3,48 +3,48 @@
 /**
  * parsing_user_input - split an input into tokens
  * @line: string - string written by the user
+ *
  * Return: array of strings - array of every arguments
  */
 
 char **parsing_user_input(char *line)
 {
-	int i = 0, len = 0, size_of_token = 80;
-	char *ptr, *end;
-	char **tokens, **tmp;
+	int size_of_token = 80;
+	int i = 0, len = 0;
+	char *token;
+	char **tokens;
 
 	tokens = malloc(sizeof(char *) * size_of_token);
+
+	if (tokens == NULL)
+	{
+		fprintf(stderr, "Allocation failed");
+		exit(EXIT_FAILURE);
+	}
+
 	len = strlen(line);
 	if (len > 0 && line[len - 1] == '\n')
 		line[len - 1] = '\0';
 
-	if (tokens == NULL)
-		exit(EXIT_FAILURE);
-	ptr = line;
-	while (*ptr != '\0')
+	token = strtok(line, " ");
+
+	if (token == NULL)
 	{
-		while (*ptr == ' ' || *ptr == '\t')
-			ptr++; /*ignores extra spaces*/
-		if (*ptr == '\0') /*end of line*/
-			break;
+		tokens[0] = NULL;
+		return (tokens);
+	}
+
+	while (token != NULL)
+	{
+		tokens[i] = token;
+		i++;
+		token = strtok(NULL, " ");
+
 		if (i >= size_of_token) /*if the buffer isn't big enough, realloc*/
 		{
 			size_of_token += 80;
-			tmp = realloc(tokens, size_of_token * sizeof(char *));
-			if (tmp == NULL)
-				exit(EXIT_FAILURE);
-			tokens = tmp;
+			tokens = realloc(tokens, size_of_token * sizeof(char *));
 		}
-		tokens[i] = ptr;
-		i++;
-
-		end = strpbrk(ptr, " \t"); /*looks for separator after a token*/
-		if (end) /*if separator found*/
-		{
-			*end = '\0';
-			ptr = end + 1;
-		}
-		else
-			break;
 	}
 	tokens[i] = NULL;
 	return (tokens);
